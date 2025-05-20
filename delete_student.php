e785b3a41eb144ba5b134bd8c5fbc4f3b44d82c6
Rajan Poudel <raajan.works@gmail.com>
2025-05-20 09:49:54 +0545

    Update delete_student.php

--- delete_student.php
+++ delete_student.php
@@ -5,7 +5,8 @@ $password = "*rajan12345#";
 $dbname = "college";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
+
+ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
@@ -15,7 +16,9 @@ $sql = "DELETE FROM students WHERE roll='$roll'";
 
 if ($conn->query($sql) === TRUE) {
     echo "Record deleted successfully!";
-} else {
+} 
+
+else {
     echo "Error: " . $conn->error;
 }
 
