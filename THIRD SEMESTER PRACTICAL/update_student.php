3a2cb13aa407be4fa52e702d25c5c68480ea5eb8
Rajan Poudel <raajan.works@gmail.com>
2026-06-15 20:11:39 +0545

    Remove database password from update_student.php
    Remove the database password for security reasons.

--- THIRD SEMESTER PRACTICAL/update_student.php
+++ THIRD SEMESTER PRACTICAL/update_student.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "*rajan12345#";
+$password = "";
 $dbname = "college";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
